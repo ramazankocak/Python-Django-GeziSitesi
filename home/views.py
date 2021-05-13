db3b16f2b571361e9e7ed9a8cf7a139992c7918e
alp-rk <raki__1@hotmail.com>
2021-05-14 00:47:04 +0300

    upload

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    text="MerHaBa"
+    return HttpResponse("DosTuM %s" % text)
