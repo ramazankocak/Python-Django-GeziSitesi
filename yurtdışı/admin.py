a8076e2e66d768085476b82cb7f9b1df7378ebee
alp-rk <raki__1@hotmail.com>
2021-05-16 16:53:32 +0300

    admin

--- yurtdışı/admin.py
+++ yurtdışı/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from yurtdışı.models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','status']
+    list_filter = ['status']
+
+admin.site.register(Category,CategoryAdmin)
--- yurtiçi/admin.py
+++ yurtiçi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from yurtiçi.models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','status']
+    list_filter = ['status']
+
+admin.site.register(Category,CategoryAdmin)
